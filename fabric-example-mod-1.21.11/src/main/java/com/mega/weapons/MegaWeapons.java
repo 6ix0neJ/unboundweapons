0c2b30032b4a1e8f0512d5530bf63c449d69448b
thamallot <thamallot05@gmail.com>
2026-05-13 22:57:00 -0400

    Initial commit

---
+++ fabric-example-mod-1.21.11/src/main/java/com/mega/weapons/MegaWeapons.java
@@ -0,0 +1,12 @@
+package com.mega.weapons;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
+public class MegaWeapons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Mega Weapons Mod Initialized!");
+    }
+}
